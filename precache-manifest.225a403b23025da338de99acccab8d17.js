ff765354fc2a3d78648cfbe0851c5f5de4c1376d
Henrique Freire <h.freire02@gmail.com>
2019-11-12 23:17:44 -0300

    Updates

---
+++ precache-manifest.225a403b23025da338de99acccab8d17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cc2b0d9b7abca09f085604c5ef93378",
+    "url": "/ihctrabalhofinal/index.html"
+  },
+  {
+    "revision": "9116edc003a3b5516c78",
+    "url": "/ihctrabalhofinal/static/css/main.0163ff21.chunk.css"
+  },
+  {
+    "revision": "90e145500e16361eb2ec",
+    "url": "/ihctrabalhofinal/static/js/2.343ed0bb.chunk.js"
+  },
+  {
+    "revision": "9116edc003a3b5516c78",
+    "url": "/ihctrabalhofinal/static/js/main.6349b291.chunk.js"
+  },
+  {
+    "revision": "9d59c9abece4adc5b192",
+    "url": "/ihctrabalhofinal/static/js/runtime-main.e9ba09e9.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ihctrabalhofinal/static/media/logo.25bf045c.svg"
+  }
+]);
